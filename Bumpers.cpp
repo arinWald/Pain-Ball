4d4c6387969096ef74d4bc02d84680ca6f1483be
Urii98 <99950184+Urii98@users.noreply.github.com>
2022-11-10 13:32:07 +0100

    retoques

--- Bumpers.cpp
+++ Bumpers.cpp
@@ -26,6 +26,15 @@ bool Bumpers::Start()
 	PhysBody* pbodyBC4;
 	pbodyBC4 = App->physics->CreateCircle(173, 96, 10, b2_staticBody);
 
+	BumperElement bigCircle1{ "bigCircle1", pbodyBC1 };
+	bumpersList.add(&bigCircle1);
+	BumperElement bigCircle2{ "bigCircle2", pbodyBC2 };
+	bumpersList.add(&bigCircle2);
+	BumperElement bigCircle3{ "bigCircle3", pbodyBC3 };
+	bumpersList.add(&bigCircle3);
+	BumperElement bigCircle4{ "bigCircle4", pbodyBC4 };
+	bumpersList.add(&bigCircle4);
+
 	//Bumpers - Small Circles
 	PhysBody* pbodySC1;
 	pbodySC1 = App->physics->CreateCircle(63, 217, 8, b2_staticBody);
@@ -34,6 +43,12 @@ bool Bumpers::Start()
 	PhysBody* pbodySC3;
 	pbodySC3 = App->physics->CreateCircle(73, 241, 8, b2_staticBody);
 
+	BumperElement smallCircle1{ "smallCircle1", pbodySC1 };
+	bumpersList.add(&smallCircle1);
+	BumperElement smallCircle2{ "smallCircle2", pbodySC2 };
+	bumpersList.add(&smallCircle2);
+	BumperElement smallCircle3{ "smallCircle3", pbodySC3 };
+	bumpersList.add(&smallCircle3);
 
 	//Bumpers - Triangles
 	PhysBody* pbodyT1;
@@ -41,8 +56,6 @@ bool Bumpers::Start()
 
 
 
-	BumperElement bigCircle1{ "bigCircle1", pbodyBC1 };
-	bumpersList.add(&bigCircle1);
 
 
 	return true;
