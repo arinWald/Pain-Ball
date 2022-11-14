976ab352e1f7d9d9efb1af1ceec917b7f0ea6241
Arnau González Acosta <arnau.gonzalez.acosta@estudiantat.upc.edu>
2022-11-14 08:21:42 +0100

    SetDensity Ball

--- Ball.cpp
+++ Ball.cpp
@@ -5,6 +5,7 @@
 #include "ModuleInput.h"
 #include "SDL\include\SDL.h"
 #include <iostream>
+using namespace std;
 
 Ball::Ball(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -16,8 +17,14 @@ Ball::~Ball()
 // Load assets
 bool Ball::Start()
 {
-	//*ball.get() = App->physics->CreateCircle(303, 767, 10, b2_dynamicBody);
-	ball= App->physics->CreateCircle(194, 263, 7, b2_dynamicBody);
+	ball = App->physics->CreateCircle(194, 263, 7, b2_dynamicBody);
+	ball->body->GetFixtureList()->SetDensity(10.0f);
+
+	//App->physics->CreateRectangle(300, 416, 600, 50, b2_staticBody);
+
+	ball->body->ResetMassData();
+	/*ball->body->SetGravityScale(2.0f);*/
+
 	LOG("Loading Ball");
 	return true;
 }
@@ -33,8 +40,9 @@ bool Ball::CleanUp()
 // Update: draw background
 update_status Ball::Update()
 {
-	std::cout << "Mouse X - " << App->input->GetMouseX() << std::endl;
-	std::cout << "Mouse Y - " << App->input->GetMouseY() << std::endl;
+	//std::cout << "Mouse X - " << App->input->GetMouseX() << std::endl;
+	//std::cout << "Mouse Y - " << App->input->GetMouseY() << std::endl;
+	cout << ball->body->GetMass() << endl;
 	
 	return UPDATE_CONTINUE;
 }
